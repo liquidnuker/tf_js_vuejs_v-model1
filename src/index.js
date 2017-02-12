bd82f0cfd528a0fbba471ea21b9d1fd869700d46
administrator <liquidnuker@users.noreply.github.com>
2017-02-11 18:00:02 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import "./js/vendor/vue.min.js";
+// import Vue from 'vue';
+// var Vue = require("./js/vendor/vue.min.js"); // ok
+const Vue = require("./js/vendor/vue.min.js");
+ 
+let app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: "message1",
+    message2: "message2"
+  }
+});
